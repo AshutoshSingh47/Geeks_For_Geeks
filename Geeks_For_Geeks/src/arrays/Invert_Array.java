ba13d66e5d303047030fac1fbb3e01827e1d0c60
RAJ <RAJ@p-PC>
2020-07-19 11:20:18 +0530

    Second Commit

--- Geeks_For_Geeks/src/arrays/Invert_Array.java
+++ Geeks_For_Geeks/src/arrays/Invert_Array.java
@@ -1,23 +1,28 @@
 package arrays;
-
+import java.util.Scanner;
 public class Invert_Array {
-
 	public static void main(String[] args) {
-		int i,j;
-		i=0;
+		Scanner sc=new Scanner(System.in);
+		int i,j,n;
 		int a[]={1,2,3,4,5};
-		int n=a.length;
-		System.out.println(n);
-	/*	for(i=0,j=n-1;i<n;i++,j--)
-		{
-			a[i]=a[j];
-		}*/
-	
-		}
-		/*for(int o:a)
-		{
-			System.out.print(o+" ");
-		}*/
+		int size=a.length;
+		System.out.println("Enter the limit");
+		n=sc.nextInt();
+		int b[]=new int[n];
+        for(i=0;i<n;i++){
+        	b[i]=a[i];
+        }
+        for(i=0,j=n;i<n+1;i++,j++){
+        	a[i]=a[j];
+        }
+        for(i=0,j=n+1;i<n;i++,j++){
+        	a[j]=b[i];
+        }
+        for(int e:a)
+        	System.out.print(e+" ");
+     
+//        for(int e:b)
+//        System.out.print(e+" ");
 	}
-
+}
 
