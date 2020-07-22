44c4672a332d20d7cc00dcf4e5c394f299fe7789
Ashish Singh <Ashish Singh@DESKTOP-RLMLMJT>
2020-07-22 22:41:18 +0530

    Ashish : Committed on 22/07/2020 at 10:41 P.M.

--- Geeks_For_Geeks/src/arrays/Kth_element.java
+++ Geeks_For_Geeks/src/arrays/Kth_element.java
@@ -5,10 +5,35 @@
 //If given user input 4 : That means 4th largest no in the array : That is 5.
 package arrays;
 
+import java.util.Scanner;
+
 public class Kth_element {
 
 	public static void main(String[] args) {
-		
+		int i,j,size,temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter size");
+		size=sc.nextInt();
+		int a[]=new int[size];
+		System.out.println("enter elements");
+		for(i=0;i<size;i++)
+		{a[i]=sc.nextInt();}
+		for(i=0;i<size-1;i++)
+		{
+			for(j=0;j<size-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		for(int e:a)
+		{
+			System.out.print(e+" ");
+		}
 
 	}
 
