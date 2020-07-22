f8dad3d3c1ad47f483d5de138708aa6a872a8b3e
Ashish Singh <Ashish Singh@DESKTOP-RLMLMJT>
2020-07-22 22:38:10 +0530

    Ashish : Committed on 22/07/2020 at 10:37 P.M.

--- Geeks_For_Geeks/src/arrays/Bubble_Sort.java
+++ Geeks_For_Geeks/src/arrays/Bubble_Sort.java
@@ -1,10 +1,33 @@
 package arrays;
-
+import java.util.Scanner;
 public class Bubble_Sort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i,j,size,temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter size");
+		size=sc.nextInt();
+		int a[]=new int[size];
+		System.out.println("enter elements");
+		for(i=0;i<size;i++)
+		{a[i]=sc.nextInt();}
+		for(i=0;i<size-1;i++)
+		{
+			for(j=0;j<size-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		for(int e:a)
+		{
+			System.out.print(e+" ");
+		}
+			
 	}
 
 }
