4cccaf1634ad2dbc7a19772e0b9d6b34aad69654
ashutosh <ashutoshsingh.iimt@gmail.com>
2021-10-21 20:19:50 +0530

    modified

--- Geeks_For_Geeks/src/arrays/Binary_Search.java
+++ Geeks_For_Geeks/src/arrays/Binary_Search.java
@@ -7,7 +7,7 @@ class Binary_Search {
 	{ int size;
 	size=arr.length;
 	System.out.println(size);
-		int l = 0, r = arr.length - 1; 
+		int l = 0, r = size - 1; 
 		while (l <= r) { 
 			int m = l + (r - l) / 2; 
 System.out.println("Middle Element = "+m);
