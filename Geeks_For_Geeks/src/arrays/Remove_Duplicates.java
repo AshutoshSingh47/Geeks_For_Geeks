d4b33b5898d3c793049b7c28bdc053e3f084cb64
muske <ashutoshsingh.iimt@gmail.com>
2022-07-28 18:17:22 +0530

    modified time constraint

--- Geeks_For_Geeks/src/arrays/Remove_Duplicates.java
+++ Geeks_For_Geeks/src/arrays/Remove_Duplicates.java
@@ -1,30 +1,27 @@
 package arrays;
 
 public class Remove_Duplicates {
-	public static void remove_duplicate(int A[],int N){
+	public static int remove_duplicate(int A[],int N){
         // code here
-        int B[]=new int[A[N-1]+1];
-        System.out.println(A[N-1]+" "+B.length);
-        int count=0;
-        if(N==1)
-        {
-           System.out.println();
-        }
-        for(int i=0;i<N;i++)
-        {
-            B[A[i]]++;
-        }        
-        for(int i=0;i<B.length;i++)
-        {
-            if(B[i]>=1)
-            count++;
-        }
-        System.out.println(count);
+		int j=0;
+		for(int i=0;i<N;i++)
+		{
+			if(A[j]!=A[i])
+			{
+				j++;
+				A[j]=A[i];
+			}
+		}
+		return j+1;
      
     }
 	public static void main(String[] args) {
-		int arr[]={1,1 ,2 ,2 ,20, 21, 22, 23, 24, 25 };
-		remove_duplicate(arr,arr.length);
+		int arr[]={1,1 ,2 ,2 ,3,3,4,5,6,7 };
+		int size=remove_duplicate(arr,arr.length);
+		for(int i=0;i<size;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
 	}
 
 }
