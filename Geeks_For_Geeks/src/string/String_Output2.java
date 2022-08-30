dbd4ac58d16379ff7834e69f6c57c4e19d459d8b
muske <ashutoshsingh.iimt@gmail.com>
2022-08-30 17:20:05 +0530

    committed

---
+++ Geeks_For_Geeks/src/string/String_Output2.java
@@ -0,0 +1,16 @@
+package string;
+
+public class String_Output2 {
+
+	public static void main(String[] args) {
+		String s="Geeks";
+		char str[]=new char[219];
+		s.toLowerCase();
+		for(char c='A';c<='z';c++)
+		{
+		}
+		
+		
+	}
+
+}
